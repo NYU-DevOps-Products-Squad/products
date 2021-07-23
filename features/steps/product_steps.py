291bc1e1c742681ef4149d2e01fca5d1643eb99d
AmerGong <76582764+AmerGong@users.noreply.github.com>
2021-07-24 01:16:18 +0800

    Update product_steps.py

--- features/steps/product_steps.py
+++ features/steps/product_steps.py
@@ -14,14 +14,14 @@ def step_impl(context):
     """ Delete all Products and load new ones """
     headers = {'Content-Type': 'application/json'}
     # list all of the products and delete them one by one
-    context.resp = requests.get(context.base_url + '/api/products')
+    context.resp = requests.get(context.base_url + '/products')
     expect(context.resp.status_code).to_equal(200)
     for product in context.resp.json():
-        context.resp = requests.delete(context.base_url + '/api/products/' + str(product["id"]), headers=headers)
+        context.resp = requests.delete(context.base_url + 'products/' + str(product["id"]), headers=headers)
         expect(context.resp.status_code).to_equal(204)
 
     # load the database with new products
-    create_url = context.base_url + '/api/products'
+    create_url = context.base_url + '/products'
     for row in context.table:
         data = {
             "name": row['name'],
